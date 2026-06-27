35eb6326d14d24c4b6f7066e3378a624c9e1469c
Mohtashim-coder <alammohtashim3@gmail.com>
2026-06-27 10:59:11 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        
+        if(s1.length() > s2.length())
+        {
+            return false;
+        }
+
+        vector<int> count1(26, 0), count2(26, 0);
+
+        // Frequency of s1
+        for(char c : s1)
+        {
+            count1[c - 'a']++;
+        }
+
+        // First window of size n in s2
+        for(int i=0; i<s1.length(); i++)
+        {
+            count2[s2[i] - 'a']++;
+        }
+
+        // check if 1st window matches
+        if(count1 == count2)
+        {
+            return true;
+        }
+
+        // slide the window
+        for(int i=s1.length(); i<s2.length(); i++)
+        {
+            count2[s2[i] - 'a']++;
+            count2[s2[i - s1.length()] - 'a']--;
+
+            if(count1 == count2)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
