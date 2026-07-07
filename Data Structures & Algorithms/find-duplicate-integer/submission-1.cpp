11ae8776ca3bd974755fba555b40b314abbfdb7d
Mohtashim-coder <alammohtashim3@gmail.com>
2026-07-07 12:32:28 +0530

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        
+        // unordered_map<int, int> seen;
+        // for(int num : nums)
+        // {
+        //     if(seen.find(num) != seen.end())
+        //     {
+        //         return num;
+        //     }
+        //     seen[num]++;
+        // }
+        // return -1;
+
+        // using binary-search
+        int left = 1;
+        int right = nums.size()-1;
+
+        while(left < right)
+        {
+            int mid = left + (right - left)/2;
+            int count = 0;
+
+            for(int num : nums)
+            {
+                if(num <= mid)
+                {
+                    count++;
+                }
+            }
+
+            if(count > mid)
+            {
+                right = mid;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
