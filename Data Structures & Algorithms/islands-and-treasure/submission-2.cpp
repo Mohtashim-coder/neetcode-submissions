52ed08b5f72586c239aa4543a1d6e9cfbd843ccd
Mohtashim-coder <alammohtashim3@gmail.com>
2026-07-30 22:15:54 +0530

    Add: islands-and-treasure - submission-2

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-2.cpp
@@ -0,0 +1,136 @@
+class Solution {
+public:
+    void islandsAndTreasure(vector<vector<int>>& grid) {
+        
+        int rows = grid.size();
+        int cols = grid[0].size();
+        queue<pair<int, int>> q;
+
+        for(int i=0; i<rows; i++)
+        {
+            for(int j=0; j<cols; j++)
+            {
+                if(grid[i][j] == 0)
+                {
+                    q.push({i, j});
+                }
+            }
+        }
+
+        // Direction: Down, Up, Right, Left
+        vector<pair<int, int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int distance = 0;
+
+        while(!q.empty())
+        {
+            int size = q.size();
+            distance++;
+
+            for(int i=0; i<size; i++)
+            {
+                auto[r, c] = q.front();
+                q.pop();
+
+                // Explore 4 direction
+                for(auto[dr, dc] : dirs)
+                {
+                    int nr = r + dr;
+                    int nc = c + dc;
+
+                    if(nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == INT_MAX)
+                    {
+                        grid[nr][nc] = distance;
+                        q.push({nr, nc});
+                    }
+                }
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     int rows = grid.size();
+    //     int cols = grid[0].size();
+    //     queue<pair<int, int>> q;
+
+    //     // add all treasure cells to queue
+    //     for(int i=0; i<rows; i++)
+    //     {
+    //         for(int j=0; j<cols; j++)
+    //         {
+    //             if(grid[i][j] == 0)
+    //             {
+    //                 q.push({i, j});
+    //             }
+    //         }
+    //     }
+
+    //     // Directions: Down, Up, Right, Left
+    //     vector<pair<int, int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    //     int distance = 0;
+
+    //     // BFS from all treasure simiultaneously
+    //     while(!q.empty())
+    //     {
+    //         int size = q.size();
+    //         distance++;
+
+    //         for(int i=0; i<size; i++)
+    //         {
+    //             auto[r, c] = q.front();
+    //             q.pop();
+
+    //             // Explore 4 directions
+    //             for(auto[dr, dc] : dirs)
+    //             {
+    //                 int nr = r + dr;
+    //                 int nc = c + dc;
+
+    //                 // Check if valid land cell (INF)
+    //                 if(nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == INT_MAX)
+    //                 {
+    //                     grid[nr][nc] = distance;
+    //                     q.push({nr, nc});
+    //                 }
+    //             }
+    //         }
+
+    //     }
+    }
+};
