9a2de0aa22f90dfe9d0bbf3abc7c671d0563f2af
Mohtashim-coder <alammohtashim3@gmail.com>
2026-08-30 17:50:51 +0530

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        // character frequency array as key
+        unordered_map<string, vector<string>> mp;
+        for(string s : strs)
+        {
+            vector<int> freq(26, 0);
+            for(char c : s)
+            {
+                freq[c - 'a']++;
+            }
+
+            string key = "";
+            for(int i=0; i<26; i++)
+            {
+                key = key + to_string(freq[i]) + "#";
+            }
+            mp[key].push_back(s);
+        }
+
+        vector<vector<string>> result;
+        for(auto &pair : mp)
+        {
+            result.push_back(pair.second);
+        }
+        return result;
+    }
+};
